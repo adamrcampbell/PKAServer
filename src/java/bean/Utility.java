d38672b9fdbe3d833ebe948026d801659cd2637b
Obj3ct <campbell.adam.31@gmail.com>
2016-09-30 19:04:54 +1300

    Added Get Contacts functionality
    Added functionality for android client to call for an up to date client
    list

--- src/java/bean/Utility.java
+++ src/java/bean/Utility.java
@@ -1,6 +1,13 @@
 package bean;
 
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 /**
  *
@@ -10,7 +17,7 @@ public class Utility {
 
     public static byte[] decodeFromBase64(String cipher) {
         // HTML decode from transport
-        cipher = cipher.replace("%2B", "+").replace("%2F", "/");
+        cipher = cipher.replace("%2B", "+").replace("%2F", "/").replace("%3D", "=");
         // Base64 decode
         return Base64.getDecoder().decode(cipher);
     }
@@ -19,6 +26,42 @@ public class Utility {
         // Encode bytes into base64
         String encodedData = Base64.getEncoder().encodeToString(data);
         // HTML encode for transport
-        return encodedData.replace("+", "%2B").replace("/", "%2F");
+        return encodedData.replace("+", "%2B").replace("/", "%2F").replace("=", "%3D");
+    }
+    
+    public static byte[] encryptRSA(Key key, byte[] data) {
+
+        byte[] encrypted = null;
+
+        try {
+            Cipher rsaCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            rsaCipher.init(Cipher.ENCRYPT_MODE, key);
+            encrypted = rsaCipher.doFinal(data);
+        } catch (NoSuchAlgorithmException e) {
+        } catch (InvalidKeyException e) {
+        } catch (NoSuchPaddingException e) {
+        } catch (BadPaddingException e) {
+        } catch (IllegalBlockSizeException e) {
+        }
+
+        return encrypted;
+    }
+
+    public static byte[] decryptRSA(Key key, byte[] data) {
+
+        byte[] decrypted = null;
+
+        try {
+            Cipher rsaCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            rsaCipher.init(Cipher.DECRYPT_MODE, key);
+            decrypted = rsaCipher.doFinal(data);
+        } catch (NoSuchAlgorithmException e) {
+        } catch (InvalidKeyException e) {
+        } catch (NoSuchPaddingException e) {
+        } catch (BadPaddingException e) {
+        } catch (IllegalBlockSizeException e) {
+        }
+
+        return decrypted;
     }
 }
